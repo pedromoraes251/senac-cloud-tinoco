abd746896adfd53150563bfcba7dd94ad0a8d7bc
pedromoraes <henriquepedro10@outlook.com>
2022-12-06 13:17:23 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//console.log("Hello CodeSandbox");
+
+const express = require("express");
+
+const server = express();
+
+server.get("/ping", (req, res) => {
+  return res.json({ ping: "pong" });
+});
+
+server.use(express.json());
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("server on");
+});
